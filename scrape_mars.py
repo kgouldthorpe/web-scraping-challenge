a400009fd2592e5f38d07d593f2b75066e485f49
Kendall <kendall.gouldthorpe@gmail.com>
2019-09-15 14:04:49 -0400

    Homework 9/15

---
+++ scrape_mars.py
@@ -0,0 +1,49 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from splinter import Browser\n",
+    "from bs4 import BeautifulSoup\n",
+    "import pandas as pd\n",
+    "import requests"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Define the executable path specific to me\n",
+    "def init_browser():\n",
+    "    executable_path = {'executable_path': 'C:\\Program Files\\ChromeDriver\\chromedriver.exe'}\n",
+    "    return Browser(\"chrome\", **executable_path, headless = False)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
